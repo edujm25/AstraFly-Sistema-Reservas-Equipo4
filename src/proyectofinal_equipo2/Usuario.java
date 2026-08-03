789eeaccf3a85b198b87aea492235336fcc477f6
edujm25 <edwisjimenezm25@gmail.com>
2026-08-03 00:21:18 -0400

    Se crea la case Usuario y vuelo

--- src/proyectofinal_equipo2/Usuario.java
+++ src/proyectofinal_equipo2/Usuario.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectofinal_equipo2;
 
-/**
- *
- * @author Edwis Jimenez
- */
-public class Usuario {
+public class Usuario {//CLASE
+    private int idUsuario;
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private String rol;
     
+    //CONSTRUCTOR\\
+    public Usuario(int idUsuario, String nombre, String correo, String contrasena, String rol){
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contrasena = contrasena;
+        this.rol = rol;
+    }
+    
+    //Getters y Setters\\
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasenaHash) {
+        this.contrasena = contrasenaHash;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
 }
--- src/proyectofinal_equipo2/Vuelo.java
+++ src/proyectofinal_equipo2/Vuelo.java
@@ -1,13 +1,93 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package proyectofinal_equipo2;
 
-/**
- *
- * @author Edwis Jimenez
- */
-public class Vuelo {
+import java.time.LocalDateTime;
+import java.math.BigDecimal;
+
+public class Vuelo {//CLASE
+    private int idVuelo;
+    private String origen;
+    private String destino;
+    private LocalDateTime fechaSalida;
+    private BigDecimal precio;
+    private int asientosTotales;
+    private int asientosDisponibles;
+    private String estado;
     
+    //CONSTRUCTOR\\
+    public Vuelo(int idVuelo, String origen, String destino, LocalDateTime fechaSalida, BigDecimal precio,
+                 int asientosTotales, int asientosDisponibles, String estado) {
+        this.idVuelo = idVuelo;
+        this.origen = origen;
+        this.destino = destino;
+        this.fechaSalida = fechaSalida;
+        this.precio = precio;
+        this.asientosTotales = asientosTotales;
+        this.asientosDisponibles = asientosDisponibles;
+        this.estado = estado;
+    }
+    
+    //Getters y Setters
+    public int getIdVuelo() {
+        return idVuelo;
+    }
+
+    public void setIdVuelo(int idVuelo) {
+        this.idVuelo = idVuelo;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public LocalDateTime getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(LocalDateTime fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public int getAsientosTotales() {
+        return asientosTotales;
+    }
+
+    public void setAsientosTotales(int asientosTotales) {
+        this.asientosTotales = asientosTotales;
+    }
+
+    public int getAsientosDisponibles() {
+        return asientosDisponibles;
+    }
+
+    public void setAsientosDisponibles(int asientosDisponibles) {
+        this.asientosDisponibles = asientosDisponibles;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
